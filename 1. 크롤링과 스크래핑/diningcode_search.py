a0275d88154f6fe0ab4c2abec41cae88f765fbe7
kevin <kevin.lee@mangoplate.com>
2017-11-13 12:31:50 +0900

    다이닝코드 크롤링 소스 올리기

---
+++ 1. 크롤링과 스크래핑/diningcode_search.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+ 
+# 드라이버 추출하기 
+driver = webdriver.Chrome()
+
+# 검색 페이지 로딩 
+print("## 페이지 로딩 ## ")
+keyword = "강남역"
+path = "https://www.diningcode.com/isearch.php?query=" + keyword
+driver.get(path)
+driver.implicitly_wait(3)
+
+# XCode Element 추출 및 프린트 
+tags = driver.find_elements_by_xpath("//*[@id='main_area']/div/div/div/div/div/a")
+print("1. TAG Count : " , len(tags))
+
+# 추천 카테고리로 가져오기 
+print("## '", keyword, "' 추천 카테고리")
+for tag in tags :
+    print("\t - ", tag.text)
+
+print("## 추천 레스토랑 ")
+resturant_names = driver.find_elements_by_xpath("//*[@id='main_area']/div/div/div/div/div/div/a")
+for name in resturant_names :
+    print("\t - ", name.text)
